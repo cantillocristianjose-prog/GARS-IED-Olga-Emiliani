7772bd667c0ac2011091b2698c77c6c9a862286f
Littleroky <cantillocristianjose@gmail.com>
2026-09-10 19:54:39 -0500

    Api aplicada

--- pagina_web/rxconfig.py
+++ pagina_web/rxconfig.py
@@ -2,9 +2,14 @@ import reflex as rx
 
 config = rx.Config(
     app_name="pagina_web",
+    cors_allowed_origins=[
+        "https://localhost:3000",
+        "https://gars-web.vercel.app"
+    ],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
         rx.plugins.RadixThemesPlugin(),
-    ]
+    ],
+    api_url="http://apithe.up.railway.app"
 )
