62a17d64d58ec49c618d8c9d54eecffc11310aca
Littleroky <cantillocristianjose@gmail.com>
2026-09-10 20:00:24 -0500

    correcion de la web

--- pagina_web/build.sh
+++ pagina_web/build.sh
@@ -5,7 +5,8 @@ pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
 reflex export --frontend-only
+Remove-Item -Path "public" -Recurse -Force
 mkdir public
 Expand-Archive -Path "frontend.zip" -DestinationPath "public" -Force
-rm -f frontend.zip
+Remove-Item -Path "frontend.zip" -Recurse -Force
 deactivate
